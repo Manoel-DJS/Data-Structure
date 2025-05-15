8a859ea3e88fe50ea6b5eb5f97e10cb254a75d5b
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2025-05-15 11:26:01 -0300

    feat: Quest02 - Estatísticas com vetores

---
+++ Vetores/src/main/java/Vetores/me/Quest02Vector.java
@@ -0,0 +1,20 @@
+package Vetores.me;
+
+/**
+ 2. Estatísticas com Vetores - Média, Maior e Menor:
+ Desenvolva um programa que leia um vetor com 20 números reais e exiba:
+
+ A média dos números.
+
+ O maior valor e sua posição no vetor.
+
+ O menor valor e sua posição no vetor.
+
+ Desafio Extra: Permita que o usuário escolha o tamanho do vetor (máximo de 100).
+ */
+
+public class Quest02Vector {
+    public static void main(String[] args) {
+
+    }
+}
