c75b5e64016d1269774496abd46b959f57520f20
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2025-05-15 10:50:22 -0300

    feat: solução da quest01 sobre vetores

---
+++ Vetores/src/main/java/Vetores/me/Quest01Vector.java
@@ -0,0 +1,54 @@
+package Vetores.me;
+
+
+/**
+
+ 1. Manipulação de Vetores - Inversão de Ordem:
+ Crie um programa que receba um vetor de números inteiros e inverta a ordem dos elementos.
+
+ Entrada: Um vetor com 10 números inteiros. OK
+
+ Saída: O vetor com os números na ordem inversa. OK
+
+ Desafio Extra: Não utilize um vetor auxiliar.
+
+ **/
+
+public class Quest01Vector {
+    public static void main(String[] args) {
+        // criar Vetor
+        int[] vetorVazio = new int[10];
+
+        int[] numeros = {1,2,3,4,5,6,7,8,9,10};
+
+        for(int contador : numeros) {
+            System.out.printf("%d ", contador);
+        }
+
+        System.out.println("\nLista invertida");
+
+        for(int i = 9; i >= 0; i--){
+            System.out.printf("%d ", numeros[i]);
+        }
+
+        System.out.println("Inverter numeros dentro do vetor");
+        int x=0;
+        for(int i = 9; i >= 0; i--){
+            vetorVazio[x] = numeros[i];
+            x++;
+        }
+
+        System.out.println("print final");
+        printVector(vetorVazio);
+
+
+        System.out.printf("\n Hello and welcome!");
+
+    }
+
+    static public void printVector(int[] vector){
+        for(int contador : vector){
+            System.out.println(contador);
+        }
+    }
+}
