fdc7b66404012c96925353ba6b9f7f78e157b63d
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2025-05-21 08:43:29 -0300

    feat: Questão de validação de email

---
+++ Regex/src/main/java/Regex/me/Quest01Regex.java
@@ -0,0 +1,21 @@
+package Regex.me;
+
+/**
+
+        Validação de Email:
+ Escreva uma expressão regular que valide emails no formato:
+    Deve começar com letras, números ou os caracteres . e _.
+
+    Deve ter um símbolo @.
+
+    Após o @, deve haver um domínio (letras e números).
+
+    O domínio deve ser seguido por um ponto (.) e uma extensão de 2 a 4 letras.
+
+ */
+
+public class Quest01Regex {
+    public static void main(String[] args) {
+
+    }
+}
