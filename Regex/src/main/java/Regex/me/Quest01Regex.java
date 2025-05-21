e024acee14717ae6a73d7a209946c60d12dca1fc
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2025-05-21 11:25:53 -0300

    test: Alguns testes de regex para validação de email

--- Regex/src/main/java/Regex/me/Quest01Regex.java
+++ Regex/src/main/java/Regex/me/Quest01Regex.java
@@ -1,12 +1,15 @@
 package Regex.me;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
 
         Validação de Email:
  Escreva uma expressão regular que valide emails no formato:
-    Deve começar com letras, números ou os caracteres . e _.
+    Deve começar com letras, números ou os caracteres . e _. X
 
-    Deve ter um símbolo @.
+    Deve ter um símbolo @. OK
 
     Após o @, deve haver um domínio (letras e números).
 
@@ -16,6 +19,15 @@ package Regex.me;
 
 public class Quest01Regex {
     public static void main(String[] args) {
+        String emailFalso = "asdiodhqaefhaofaedahjw";
+        String email = "example123@gmail.com";
+        String outroEmail = "dickvigarista332@gmail.com";
+        String test = "Xextssss@gmail.com";
 
+        // Testando
+        Pattern pattern = Pattern.compile("^[a-zA-Z]{6,}[0-9]{1,6}?[@]gmail\\.com$"); // Expressão Regular
+        Matcher matcher = pattern.matcher(emailFalso); // Verificação de validação
+
+        System.out.println(matcher.find());
     }
 }
