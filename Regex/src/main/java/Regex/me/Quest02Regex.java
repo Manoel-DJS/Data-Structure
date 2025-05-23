3a1ecd3513ea8fdc7909bdfa677a25bb1bb270f9
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2025-05-22 21:23:24 -0300

    feat: Quest02

---
+++ Regex/src/main/java/Regex/me/Quest02Regex.java
@@ -0,0 +1,20 @@
+package Regex.me;
+
+/**
+
+        Captura de Datas no Formato DD/MM/AAAA:
+ Crie uma expressão regular para capturar datas no formato dd/mm/aaaa, onde:
+
+     dd representa o dia (de 01 a 31).
+
+     mm representa o mês (de 01 a 12).
+
+     aaaa representa o ano (quatro dígitos).
+
+ */
+
+public class Quest02Regex {
+    public static void main(String[] args) {
+
+    }
+}
